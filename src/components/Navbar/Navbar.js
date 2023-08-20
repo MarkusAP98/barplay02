ed9e6cfec9274dd99e9d512fa7be302227ad0363
Markus Phantha <markus.a.phantha@gmail.com>
2023-08-20 15:17:23 +0300

    out the links in the center and added margins ect.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,8 +3,34 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar">
+      <ul className="barplay__navbar-links">
+        <li className="barplay__links">
+          <a>Kokoustilat</a>
+        </li>
+        <li className="barplay__links">
+          <a>Bar Play Joensuu</a>
+        </li>
+        <li className="barplay__links">
+          <a>Ruokalista</a>
+        </li>
+      </ul>
+      <div className="navbar_icons">
+        <ul>
+          <li>
+            <a>Fb</a>
+          </li>
+          <li>
+            <a>Ig</a>
+          </li>
+          <li>
+            <a>Viesti</a>
+          </li>
+        </ul>
+      </div>
+      <button>Varaa aikasi</button>
+    </div>
+  );
 }
 
 export default Navbar
