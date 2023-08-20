6067542fb9ca5dac45a8e04ad8dc4dda7b27f5e8
Markus Phantha <markus.a.phantha@gmail.com>
2023-08-20 17:54:14 +0300

    added some images and got the main img working

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Main.css'
+import myImage from "../../assets/Main.jpg";
 
 const Main = () => {
   return (
-    <div>Main</div>
-  )
+    <div>
+      <h1>gdvcgdc</h1>
+      <img src={myImage} alt=''></img>
+      </div>
+  );
 }
 
 export default Main
