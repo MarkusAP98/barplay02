429e9e3996f4d21b08c1d8813db6bcf74644dc74
Markus Phantha <markus.a.phantha@gmail.com>
2023-08-21 19:30:32 +0300

    got some stuff done with navbar and main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import './Main.css'
+import React from "react";
+import "./Main.css";
 import myImage from "../../assets/Main.jpg";
-import logo from '../../assets/logo.webp'
+import logo from "../../assets/logo.webp";
 
 const Main = () => {
   return (
-    <div className='barplay__main'>
-      <img src={myImage} alt="" className='barplay__main-img'>
-      </img>
-        <img src={logo} className='barplay__logo'/>
+    <div className="barplay__main">
+      <img src={myImage} className="barplay__main-img" />
+      <img src={logo} className="barplay__logo" />
     </div>
   );
-}
+};
 
-export default Main
+export default Main;
