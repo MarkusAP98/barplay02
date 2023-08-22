b2453d20fab68592b1c147b71e876c6589c586bc
Markus Phantha <markus.a.phantha@gmail.com>
2023-08-22 20:41:26 +0300

    made the main img look better

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,36 +1,40 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <ul className="barplay__navbar-links">
-        <li className="barplay__links">
-          <a>Kokoustilat</a>
-        </li>
-        <li className="barplay__links">
-          <a>Bar Play Joensuu</a>
-        </li>
-        <li className="barplay__links">
-          <a>Ruokalista</a>
-        </li>
-      </ul>
-      <div className="navbar_icons">
-        <ul>
-          <li>
-            <a>Fb</a>
+    <>
+      <div className="navbar">
+        <ul className="barplay__navbar-links">
+          <li className="barplay__links">
+            <a>Kokoustilat</a>
           </li>
-          <li>
-            <a>Ig</a>
+          <li className="barplay__links">
+            <a>Bar Play Joensuu</a>
           </li>
-          <li>
-            <a>Viesti</a>
+          <li className="barplay__links">
+            <a>Ruokalista</a>
           </li>
         </ul>
+        <div className="navbar_icons">
+          <ul>
+            <li>
+              <a>
+                
+              </a>
+            </li>
+            <li>
+              <a>Ig</a>
+            </li>
+            <li>
+              <a>Viesti</a>
+            </li>
+          </ul>
+        </div>
+        <button>Varaa aikasi</button>
       </div>
-      <button>Varaa aikasi</button>
-    </div>
+    </>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
