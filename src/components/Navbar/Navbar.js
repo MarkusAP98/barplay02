ce925ed5da723e0043390cecd9825bea0ea1e02e
Markus Phantha <markus.a.phantha@gmail.com>
2023-08-30 19:45:49 +0300

    added smallscreen overlay

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -41,6 +41,49 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+
+        <div className="navbar__smallscreen">
+          <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+
+          <div className="barplay__navbar-smallscreen_overlay flex__center slide-bottom">
+            <RxCross2
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => {}}
+            />
+            <ul className="barplay__navbar-smallscreen-links">
+              <li className="barplay__links">
+                <a>Kokoustilat</a>
+              </li>
+              <li className="barplay__links-smallscreen">
+                <a>
+                  <p>Bar Play Joensuu</p>
+                  <p id="barplay-joensuu-smallscreen">joensuu</p>
+                </a>
+              </li>
+              <li className="barplay__links-smallscreen">
+                <a>Ruokalista</a>
+              </li>
+            </ul>
+            <ul className="barplay__navbar-smallscreen-links-icons">
+              <li>
+                <a className="navbar-icons">
+                  <AiFillFacebook />
+                </a>
+              </li>
+              <li>
+                <a className="navbar-icons">
+                  <AiFillInstagram />
+                </a>
+              </li>
+              <li>
+                <a className="navbar-icons">
+                  <AiFillMessage />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
         <button className="barplay__navbar-btn">Varaa aikasi</button>
       </nav>
     </>
