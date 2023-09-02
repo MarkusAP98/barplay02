f1e1479588b166595eed4d9343d623431737da8a
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-02 21:15:48 +0300

    made the info section look better

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,11 +1,65 @@
-import React from 'react'
-import './Info.css'
-
+import React from "react";
+import "./Info.css";
+import { AiOutlineClockCircle } from "react-icons/ai";
 
 const Info = () => {
   return (
-    <div>Info</div>
-  )
-}
+    <div className="barplay__info-section">
+      {/* -----info text section------- */}
+      <div className="barplay__info-text">
+        <h1>MUKANA PELISSÄ!</h1>
+        <p>
+          Bar Play on kaupungin menevin sporttibaari! Meillä voit katsoa
+          urheilua kolmelta jättiscreeniltä sekä kymmenesta muusta televisiosta!
+          Lisäksi baaristamme löytyy keittiö, joka tarjoaa herkullista
+          pubiruokaa joka päivä aina yöhön saakka. Juomaa luonnollisesti ihan
+          jokaiseen makuun! Meillä voit myös pelata biljardia, lautapelejä tai
+          vaikka Hohto Beer Pongia!
+        </p>
+        <br></br>
+        <li>
+          <a>Tutustu ruokalistaamme ja tee tilauksesi tästä!</a>
+        </li>
+        <br></br>
+        <h2>Asiointi perheille</h2>
+        <p>
+          Play on viikonloppuisin (pe-su) ja suurten tapahtumien yhteydessä aina
+          K18. Alaikäiset ovat kuitenkin tervetulleita vanhempien kanssa
+          ruokailemaan ja limulle arkisin (ma-to) ennen klo 18.00.
+        </p>
+        <br></br>
+        <h2>Tilavaraukset</h2>
+        <br></br>
+        <p>
+          Tilavaraukset ja tarjouspyyynnöt myyntipalvelustamme
+          sales.karelia@sokoshotels.fi
+        </p>
+        <br></br>
+        <p>
+          Muihin kyselyihin vastaamme viikon sisällä ja useimmin parissa
+          päivässä parhaiten täältä barplay.joensuu@sok.fi
+        </p>
+        <br></br>
+        <p>Lämpimästi tervetuloa!</p>
+      </div>
 
-export default Info
+      {/* -------- schedule info section ------- */}
+
+      <div className="barplay__schedule-section">
+        <div className="barplay__schedule-section-text">
+          <h1>Bar play Joensuu</h1>
+          <div className="barplay__schedule-clock">
+            <AiOutlineClockCircle className="clock-icon" />
+            <div className="barplay__schedule-clock-p">
+              <p>Tänään: 12:00 - 02:00</p>
+              <p>Keittiö: 12:00 - 01:00</p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
