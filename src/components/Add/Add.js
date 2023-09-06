6c91eab1c1b1086e77ad65e7d77d6479b76cf8d1
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-06 16:52:12 +0300

    added some text to add

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -1,20 +1,38 @@
-import React from 'react'
-import './Add.css'
+import React from "react";
+import "./Add.css";
+import Dining from "../../assets/add_section/dining.webp";
+import Bartender from '../../assets/add_section/bartender.webp'
 
 const Add = () => {
   return (
     <div className="barplay__add">
       <h1>AJANKOHTAISTA</h1>
-      <div className='add__section'>
-        <div className='add__section-1'>
-          <img></img>
+      <div className="add__section">
+        <div className="add__section-1">
+          <img src={Dining}></img>
+          <div className="add__dining-text">
+            <h2>Anna lahjaksi matka makujenmailmaan</h2>
+            <p>
+              Ravintolalahjakortilla torjoat lahjansaajalle unohtumattomia
+              makuja ja ikimuistoisia elämyksiä! Osta ravintolalahjakortti
+              lähimmästä S-ravintolasta.
+            </p>
+            <a>Lue lisää</a>
+          </div>
         </div>
-        <div>
-          <div><img></img><p></p></div>
+        <div className="add__section-2">
+          <div className="add__bartender">
+            <img src={Bartender} alt="bartender" className="bartender"></img>
+            <div className="bartender__text">
+              <h2>Asiakasomistajan omat viinit ja samppanja</h2>
+            <p>Hei viiniystävät! Vouden viinitilakierros on tehty-löysimme nautittavaksi erinomaiset asiakasomistajaviinit. Tervetuloa maistelemaan ja nauttimaan!</p>
+            <a>Lue lisää</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Add
+export default Add;
