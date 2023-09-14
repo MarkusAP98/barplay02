c8103905120d9881ef23e96a12f262751492600e
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-14 15:54:54 +0300

    imported the images and displayed them

--- src/components/images/Images.js
+++ src/components/images/Images.js
@@ -3,19 +3,21 @@ import barplay from "../../assets/images_slider/barplay.webp";
 import biljard from "../../assets/images_slider/biljard.jpg";
 import food from "../../assets/images_slider/food.webp";
 import markusBat from "../../assets/images_slider/markus bat.webp";
+import './Images.css'
 
 const Images = () => {
   // Sample image data
 
   return (
-
-     <div>
+    <div className="barplay__img-slider">
       <h1>Kuvia Ravintolasta!</h1>
-      <div className="barplay__img-slider">
-
+      <div className="img-slider">
+        <img className="image" src={barplay} />
+        <img className="image" src={biljard} />
+        <img className="image" src={food} />
+        <img className="image" src={markusBat} />
+      </div>
     </div>
-     </div>
-    
   );
 };
 
