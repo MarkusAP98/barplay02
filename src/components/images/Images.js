133a084060b78e1187a406ecb75de2a002f6601b
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-14 16:14:20 +0300

    imported images for the img-slider

--- src/components/images/Images.js
+++ src/components/images/Images.js
@@ -3,7 +3,14 @@ import barplay from "../../assets/images_slider/barplay.webp";
 import biljard from "../../assets/images_slider/biljard.jpg";
 import food from "../../assets/images_slider/food.webp";
 import markusBat from "../../assets/images_slider/markus bat.webp";
-import './Images.css'
+import nina from "../../assets/images_slider/bartender_nina.jpg";
+import burgers from "../../assets/images_slider/dble_burgers.jpg";
+import furniture_1 from "../../assets/images_slider/furniture_1.jpg";
+import furniture_2 from "../../assets/images_slider/furniture_2.jpg";
+import furniture_3 from "../../assets/images_slider/furniture_3.jpg";
+import people from "../../assets/images_slider/happy_people.jpg";
+import people_2 from "../../assets/images_slider/happyPeople_2.jpg";
+import "./Images.css";
 
 const Images = () => {
   // Sample image data
@@ -16,6 +23,13 @@ const Images = () => {
         <img className="image" src={biljard} />
         <img className="image" src={food} />
         <img className="image" src={markusBat} />
+        <img className="image" src={nina} />
+        <img className="image" src={furniture_1} />
+        <img className="image" src={furniture_2} />
+        <img className="image" src={furniture_3} />
+        <img className="image" src={burgers} />
+        <img className="image" src={people} />
+        <img className="image" src={people_2} />
       </div>
     </div>
   );
