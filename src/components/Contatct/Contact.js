500157c5582fd2d02c1fddb6c51f417fda740e4f
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-18 12:56:58 +0300

    commented for easier readability

--- src/components/Contatct/Contact.js
+++ src/components/Contatct/Contact.js
@@ -1,27 +1,36 @@
-import React from 'react'
-import './Contact.css'
-
+import React from "react";
+import "./Contact.css";
+import { AiFillFacebook, AiFillInstagram, AiFillMessage } from "react-icons/ai";
 const Contact = () => {
   return (
-    <div className='barplay__yhteistiedot'>
-    <h1>YHTEISTIEDOT</h1>
-    <div className='yhteistiedot__info'>
-        <div className='info-palautetta'>
-            <button>Anna Palautetta</button>
-            
+    <div className="barplay__yhteistiedot">
+      <h1>YHTEISTIEDOT</h1>
+      <div className="yhteistiedot__info">
+        <div className="info-palautetta">
+          {/* ------------palautetta---------- */}
+          <button className="info-btn">Anna Palautetta</button>
+          <li>
+            <a>
+              <AiFillFacebook className="info_icon-fb" />
+            </a>
+            Facebook
+          </li>
+          <li>
+            <a>
+              <AiFillInstagram className="info_icon-ig" />
+            </a>
+            Instagram
+          </li>
         </div>
-        <div className='info-karta'>
-
-        </div>
-        <div className='info-soita'>
-
-        </div>
-        <div className='info-aikataulu'>
-
-        </div>
+        {/*--------karta--------- */}
+        <div className="info-karta"></div>
+        {/* --------soita------- */}
+        <div className="info-soita"></div>
+        {/* ------------aikataulu-------- */}
+        <div className="info-aikataulu"></div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
