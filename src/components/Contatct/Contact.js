94d9d8862e3844f3c71310706191334180089144
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-18 12:47:15 +0300

    added div for ev ery info sections we needed

--- src/components/Contatct/Contact.js
+++ src/components/Contatct/Contact.js
@@ -5,6 +5,21 @@ const Contact = () => {
   return (
     <div className='barplay__yhteistiedot'>
     <h1>YHTEISTIEDOT</h1>
+    <div className='yhteistiedot__info'>
+        <div className='info-palautetta'>
+            <button>Anna Palautetta</button>
+            
+        </div>
+        <div className='info-karta'>
+
+        </div>
+        <div className='info-soita'>
+
+        </div>
+        <div className='info-aikataulu'>
+
+        </div>
+    </div>
     </div>
   )
 }
